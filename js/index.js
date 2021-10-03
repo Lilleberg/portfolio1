5624559aaaf122fb8ec4a42db73009ad26189171
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-10-03 19:34:57 +0200

    layout done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 const buttonToTop = document.querySelector(".to-top");
+const buttonMenu = document.querySelector(".hamb-menu");
+const nav = document.querySelector("nav");
 
 buttonToTop.onclick = function () {
-  console.log("click");
   document.documentElement.scrollTo({
     top: 0,
   });
+}
+
+buttonMenu.onclick = function () {
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
+
+document.querySelector("main").onclick = function () {
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  }
 }
